88358d8e9f8119fb3ff8adcffec38b262aa4b817
nnissan <nnissan@alumni.uwo.ca>
2014-03-30 14:06:17 -0400

    changed urls

--- metagenomics/urls.py
+++ metagenomics/urls.py
@@ -127,8 +127,6 @@ urlpatterns = patterns('',
     url(r'^export/contig', contig_queryset),
     url(r'^export/cosmid_endtag', cosmid_endtag_queryset),
     url(r'^export/orf_contig', orf_contig_queryset),
-    
-    url(r'^/contigtool/$', ContigTool),
-    url(r'^/pooling/$', AnnotationTool),
+   
     
 )
